a1b63b6cb9d55b25c86ccd4321b82f1dbf04e26c
John Goerzen <jgoerzen@complete.org>
2003-09-03 07:22:07 +0100

    Removed example files and fixed remaining templates
    Removed example files and fixed remaining templates
    
    
    (jgoerzen@complete.org--projects/tla-load-dirs--head--1.0--patch-13)

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the tla-load-dirs package
-#
-0 4	* * *	root	tla-load-dirs_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(tla-load-dirs):needs=X11|text|vc|wm section=Apps/see-menu-manual\
-  title="tla-load-dirs" command="/usr/bin/tla-load-dirs"
