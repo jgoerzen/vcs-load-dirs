a9997d4a39aeae0fd109e22b57f916f754377cb9
John Goerzen <jgoerzen@complete.org>
2003-12-17 05:23:58 +0100

    Fixed version detection for "candidates"; add changelog
    Keywords: 
    
     * Added "upstream" changelog (from tla), note it in debian/rules,
     and build-dep on a version of debhelper that can support it.
     * Fixed tla version detection to work with the new "candidate" versions
     in Debian sid.
    
    
    (jgoerzen@complete.org--projects/tla-load-dirs--head--1.0--patch-36)

--- tla_support/commandver.py
+++ tla_support/commandver.py
@@ -26,7 +26,9 @@ def gettlasyntax():
     if tlasyn != None:
         return tlasyn
 
-    if util.getstdoutsafeexec('tla', ['-V'])[0].find('tla-1.0.') != -1:
+    verstring = util.getstdoutsafeexec('tla', ['-V'])[0]
+    if verstring.find('tla-1.0.') != -1 and \
+            verstring.find('+1.1-candidate') == -1:
         tlasyn = '1.0'
         tlaobj = Tla10()
     else:
