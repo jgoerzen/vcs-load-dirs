113c8fb6ba11bf7de56e53615f93a5de6f062e4e
John Goerzen <jgoerzen@complete.org>
2005-04-21 09:06:04 +0100

    Preparing for 1.0.18

--- tla_support/init.py
+++ tla_support/init.py
@@ -5,7 +5,7 @@ from tla_support import util, commandver
 import sys
 
 def run(darcsdefault):
-    version = '1.0.14'
+    version = '1.0.18'
 
     parser = OptionParser(usage="usage: %prog [options] newpath",
                           version=version)
