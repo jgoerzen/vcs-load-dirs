ace13f6eac114fb7f2f86b34feedc777a8136d3e
John Goerzen <jgoerzen@complete.org>
2007-02-28 23:09:28 +0100

    Updating for 1.1.3

--- vcs_support/init.py
+++ vcs_support/init.py
@@ -21,7 +21,7 @@ from vcs_support import util, commandver
 import sys
 
 def run(darcsdefault):
-    version = '1.1.2'
+    version = '1.1.3'
 
     parser = OptionParser(usage="usage: %prog [options] vendor_source_dir",
                           version=version)
